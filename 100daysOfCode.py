28922694016c6b2a0c6d264260ab6009313d8c01
Shahid Khan <shahidkamal08@gmail.com>
2017-01-18 23:10:43 +0530

    Updated Automatic Script

---
+++ 100daysOfCode.py
@@ -0,0 +1,60 @@
+def main():
+	print("1-%s\n2-%s\n3-%s" % ("Add Log", "Add Timeline", "Exit"))
+	logFileName = "log.md"
+	timelineFileName = "timeline.md"
+	option = int(input(">>> "))
+	if option == 1:
+		autoMode = str(input("Auto Detect Last Day?(Y/N)\n>>> ")).lower()
+		if autoMode == 'y':
+			print("""
+		[*] Auto mode is only work if you cloned my repo!
+		[*] If not then just make your log.md looks like mine.
+					(@github.com/shahidkh4n)
+				""")
+			logFile = open(logFileName, "r")
+			lastDayString = logFile.readlines()[-5:-4][0]
+			print(lastDayString)
+			logFile.close()
+			import re
+			lastDay = int(re.findall(r'Day [0-9]', lastDayString)[0][-1])
+			currentDay = lastDay+1
+			day = currentDay
+			print("[+] Last Day was %s " % (lastDay))
+		else:
+			day = str(input("[-] Current Day (ex. 5)\n>>> "))	
+		template = """
+### Day {day}: {date}
+
+**Today's Progress**: {progress}
+
+**Thoughts:** {thoughts}"""
+		progress = str(input("[-] Progress (I did something 'X')\n>>> "))
+		thoughts = str(input("[-] Thoughts (What you feel about it?)\n>>> "))
+		import time
+		date = time.strftime("%B, %d %Y")
+		with open(logFileName, "a") as log:
+			log.write(template.format(day=day, date=date, progress=progress, thoughts=thoughts))
+		print("[+] Successful")
+	elif option == 2:
+		autoMode = str(input("Auto Detect Last Timeline?(Y/N)\n>>> ")).lower()
+		if autoMode == 'y':
+			lastTimeLineNumber = ""
+			with open(timelineFileName, "r") as timelineFile:
+				lastTimeLineNumber = int( timelineFile.readlines()[-1].strip().split('.')[0] )
+			currentTimeLineNumber = lastTimeLineNumber+1
+		else:
+			currentTimeLineNumber = str(input("[-] Current Time Line Number?\n>>> "))
+		
+		template = """
+	 {timelineNumber}. {timelineDescription}"""
+		timelineDescription = str(input("[-] Whats this task is all about?\n>>> "))
+		with open(timelineFileName, "a") as timelineFile:
+			timelineFile.write(template.format(timelineNumber=currentTimeLineNumber, timelineDescription=timelineDescription))
+		print("[+] Successful")
+	elif option == 3:
+		print("<> Any feedback ? Send here @twitter/sh4hidkh4n")
+		exit(0)
+
+if __name__ == "__main__":
+	while True:
+		main()
---
+++ timeline.md
@@ -0,0 +1,15 @@
+Time Line
+=========
+As I am facing hard time organizing stuffs for [#100DaysOfCode](https://twitter.com/hashtag/100DaysOfCode?src=hash "100DaysOfCode") so I am setting up a timeline for all my upcoming task. Here it goes. To make it more effective I will not set complete timeline for 100 days but the next 4 days. 
+**Updated:  12th of 100**
+
+**Day**
+ 13. Fix RecyclerView bug and complete navigation drawer.
+ 14. Implement SettingActivity and Feedback Dialog
+ 15. Fix Some Chapters Bug and Run Speed Benchmarks
+ 16. Sign Up for Playstore Developer License And Make Intro Videos Etc.
+ 17. Finalize App and make it ready for playstore
+ 18. advertise and spread all over the world
+ 19. Finish mrkhan.ml as in thoughts
+ 20. Make mrkhan.ml as SPA and use firebase for dynamic content
+ 21. Resume Crazycircle.apk 
